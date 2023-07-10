64aa16bcec09c41939b594af5c6d9b42a4211a83
yneskladovae <yneskladovae27@gmail.com>
2023-07-10 23:25:11 +0300

    feat: implement a function that get min value in row and add class

--- src/features/Table/Table.tsx
+++ src/features/Table/Table.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./Table.module.css";
 import { FIRST_ENDPOINTS, SECOND_ENDPOINTS, THIRD_ENDPOINTS } from "../../common/constants/constants";
 import { Endpoint, useLongpolling } from "../../common/hooks/useLongpolling";
 
 export const Table = () => {
+  const [loading, setLoading] = useState(true);
   const endpoints: Endpoint[] = [
     { source: "First", path: FIRST_ENDPOINTS.path, longpoll: FIRST_ENDPOINTS.longpoll },
     { source: "Second", path: SECOND_ENDPOINTS.path, longpoll: SECOND_ENDPOINTS.longpoll },
@@ -12,11 +13,17 @@ export const Table = () => {
 
   const currencyData = useLongpolling(endpoints);
 
-  const formatValue = (value: number | undefined): string => {
+  const formatValue = (value: number) => {
     if (typeof value === "number") {
       return value.toFixed(2);
     }
     return "-";
+  };
+
+  const getMinValueClass = (pair: string, source: string): string => {
+    if (!currencyData[pair]) return "";
+    const minValue = Math.min(...Object.values(currencyData[pair]));
+    return currencyData[pair][source] === minValue ? s.highlight : "";
   };
 
   return (
@@ -33,9 +40,9 @@ export const Table = () => {
         {Object.entries(currencyData).map(([pair, sources]) => (
           <tr key={pair}>
             <td>{pair}</td>
-            <td>{formatValue(sources.First)}</td>
-            <td>{formatValue(sources.Second)}</td>
-            <td>{formatValue(sources.Third)}</td>
+            <td className={getMinValueClass(pair, "First")}>{formatValue(sources.First)}</td>
+            <td className={getMinValueClass(pair, "Second")}>{formatValue(sources.Second)}</td>
+            <td className={getMinValueClass(pair, "Third")}>{formatValue(sources.Third)}</td>
           </tr>
         ))}
       </tbody>
